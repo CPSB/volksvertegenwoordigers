4da954daa0f1e853801b66557874dfd912fabf81
Tim Joosten <Tjoosten3@gmail.com>
2017-07-01 21:06:32 +0200

    init empty classes

--- src/Volksvertegenwoordigers.php
+++ src/Volksvertegenwoordigers.php
@@ -4,5 +4,38 @@ namespace ActivismeBe\Parlement;
 
 class Volksvertegenwoordigers extends Base
 {
+    public function adreslijst()
+    {
 
+    }
+
+    public function bureau()
+    {
+
+    }
+
+    public function gewezen()
+    {
+
+    }
+
+    public function huidige()
+    {
+
+    }
+
+    public function lijst()
+    {
+
+    }
+
+    public function bureauUitgebreid()
+    {
+
+    }
+
+    public function persoon($id)
+    {
+
+    }
 }
